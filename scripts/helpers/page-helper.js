e176ad189b59a644be4a6f9f35f39cce5fe1322e
小橘猫 <chan.wj@outlook.com>
2025-09-01 15:49:10 +0800

    Improved SEO
    Increase meta description characters length to improve SEO.

--- scripts/helpers/page-helper.js
+++ scripts/helpers/page-helper.js
@@ -10,6 +10,6 @@ const { stripHTML, truncate } = require("hexo-util");
 hexo.extend.helper.register('getPageDescription', function () {
   const { config, page } = this;
   let description = page.excerpt || page.content || config.description || "Hexo Theme MEOW";
-  description = truncate(stripHTML(description), { length: 150 });
+  description = truncate(stripHTML(description), { length: 200 });
   return description;
 })
